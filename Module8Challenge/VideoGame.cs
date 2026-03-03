3cdfb6ea815c33f1c094fa80ebf38fcb4ab389e0
mariegeislinger <marie.geislinger@go.minneapolis.edu>
2026-03-03 19:56:25 +0000

    Mod 8 Challenge Video Game

---
+++ Module8Challenge/VideoGame.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Filtering and Sorting A List of Video Games
+/// </summary>
+namespace VideoGames
+
+class VideoGame
+{
+    //Attributes of VideoGame
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Genre { get; set; }
+    public int Rating { get; set; }
+}
